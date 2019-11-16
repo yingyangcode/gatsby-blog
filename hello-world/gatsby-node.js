e8bfdc6e19d57066da6c17518f8e87bbf468ef23
yingyangcode <marcel.anis.21@gmail.com>
2019-11-15 21:04:38 -0500

    rendering articles

--- hello-world/gatsby-node.js
+++ hello-world/gatsby-node.js
@@ -1,5 +1,5 @@
 const { createFilePath } = require("gatsby-source-filesystem");
-
+const path = require("path");
 exports.onCreateNode=({node, getNode, actions}) => {
   const { createNodeField } = actions;
   if(node.internal.type === "MarkdownRemark") {
@@ -10,5 +10,28 @@ exports.onCreateNode=({node, getNode, actions}) => {
       value: slug
     });
   }
+}
+
+exports.createPages = ({graphql, actions}) => {
+  const {createPage} = actions;
+  return graphql(`{
+    allMarkdownRemark {
+      nodes {
+        fields {
+          slug
+        }
+      }
+    }
+  }`).then(result => {
+    result.data.allMarkdownRemark.nodes.forEach((node) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve('./src/layouts/BlogpostLayout.js'),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  }) 
 }
 
